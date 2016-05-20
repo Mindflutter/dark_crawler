c65a8a79ca8478bc8af8c1ffb16d9d5c89069c43
Igor Golyanov <thornograph@gmail.com>
2016-05-20 16:32:31 +0300

    combine methods in one megaloop

--- src/dark_crawler.py
+++ src/dark_crawler.py
@@ -22,13 +22,14 @@ def get_band_urls(letter_url, prefix):
     resp_letter = requests.get(letter_url)
     soup_letter = BeautifulSoup(resp_letter.text, 'html.parser')
     # get list of tuples (partial URL, band name)
-    band_urls = sorted([(item.get('href'), item.get_text()) for item in soup_letter.find_all('a', href=True)
+    band_urls = sorted([(urljoin(DARK_URL, item.get('href')), item.get_text())
+                        for item in soup_letter.find_all('a', href=True)
                         if item.get('href').startswith(prefix)])
     return band_urls
 
 
 def get_album_urls(single_band_url):
-    res = requests.get('http://darklyrics.com/a/ayreon.html')
+    res = requests.get(single_band_url)
     soup_single = BeautifulSoup(res.text, 'html.parser')
     albums = soup_single.findAll('div', {'class': 'album'})
     album_urls = []
@@ -38,10 +39,9 @@ def get_album_urls(single_band_url):
     return album_urls
 
 
-def get_single_album_lyrics(album_url):
+def get_single_album_lyrics(album_url, artist):
     res = requests.get(album_url)
     parsed_page = BeautifulSoup(res.text, 'html.parser')
-    # TODO: parse it further using regex (now looks like u'album: "Sometimes We Have Wings" (2008)')
     album_title_raw = parsed_page('h2')[0].get_text()
     lyrics = parsed_page.findChildren('div', {'class': 'lyrics'})[0]
     songs = {}
@@ -59,7 +59,7 @@ def get_single_album_lyrics(album_url):
     album, year = parse_album_raw(album_title_raw)
     for song, lyrics_list in songs.iteritems():
         # TODO: parse title to get track number
-        song_doc = {'artist': 'GET_ARTIST', 'album': album, 'year': year, 'title': song, 'lyrics': ''.join(lyrics_list)}
+        song_doc = {'artist': artist, 'album': album, 'year': year, 'title': song, 'lyrics': ''.join(lyrics_list)}
         result_docs.append(song_doc)
 
     return result_docs
@@ -76,11 +76,15 @@ def parse_album_raw(album_title_raw):
     return album, year
 
 
-for i in get_album_urls(''):
-    print(get_single_album_lyrics(i))
+# for i in get_album_urls(''):
+#     print(get_single_album_lyrics(i))
 
 # print(get_album_urls(''))
 #
-# for i in letter_urls:
-#     print(get_band_urls(i[0], i[1]))
+for i in letter_urls:
+    band_urls = get_band_urls(i[0], i[1])
+    for band_url in band_urls:
+        album_urls = get_album_urls(band_url[0])
+        for album_url in album_urls:
+            print(get_single_album_lyrics(album_url, band_url[1]))
 
